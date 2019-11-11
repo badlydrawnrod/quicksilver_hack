c7d0990c6c946b75dc8ef564df854db4819365d3
Rod <rod@badlydrawngames.com>
2019-11-11 16:37:28 +0000

    hack/particles even more generic loading code.

--- src/loading/loading.rs
+++ src/loading/loading.rs
@@ -20,11 +20,12 @@ pub struct Loading {
 impl Loading {
     pub fn new() -> Result<Self> {
         let mut loading: HashMap<String, Asset<Image>> = HashMap::new();
-        loading.insert("line".to_string(), Asset::new(Image::load("line.png")));
-        loading.insert(
-            "particle".to_string(),
-            Asset::new(Image::load("particle.png")),
-        );
+        for name in vec!["line", "particle"] {
+            loading.insert(
+                name.to_string(),
+                Asset::new(Image::load(name.to_string() + ".png")),
+            );
+        }
         Ok(Self {
             loading: loading,
             loaded: HashMap::new(),
@@ -36,18 +37,22 @@ impl GameState for Loading {
     fn update(&mut self, _window: &mut Window) -> Result<Action> {
         let loaded = &mut self.loaded;
         for (name, image) in &mut self.loading {
+            // If the image isn't loaded then make an attempt to load it.
             if !loaded.contains_key(name) {
                 image.execute(|image| {
+                    // The image has been loaded successfully, so save it.
                     loaded.insert(name.to_string(), image.to_owned());
                     Ok(())
                 })?;
             }
         }
 
-        let result = if self.loaded.len() < self.loading.len() {
-            Continue
-        } else {
+        let result = if self.loaded.len() == self.loading.len() {
+            // We successfully loaded everything.
             Transition(Box::new(Playing::new(self.loaded.clone())?))
+        } else {
+            // Still waiting.
+            Continue
         };
         result.into()
     }
