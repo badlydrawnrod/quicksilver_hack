919ed98f3b4a7f5efce79f5131bab2f751acf27b
Rod Hyde <rod@badlydrawngames.com>
2019-11-12 08:25:50 +0000

    hack/particles use a circular buffer for particles and remove magic.

--- src/playing/particles.rs
+++ src/playing/particles.rs
@@ -3,6 +3,13 @@ use quicksilver::graphics::{Background::Blended, Color, Image};
 use quicksilver::lifecycle::Window;
 use rand::{prelude::*, Rng};
 
+const PARTICLE_SIZE: f32 = 16.0;
+const PARTICLE_LIFE: f32 = 60.0;
+const HALF_LIFE: f32 = PARTICLE_LIFE / 2.0;
+const MIN_SPEED: f32 = 2.0;
+const MAX_SPEED: f32 = 6.0;
+const SPREAD_ANGLE: f32 = 30.0;
+
 struct Particle {
     alive: f32,
     position: Vector,
@@ -23,6 +30,7 @@ pub struct Particles {
     particles: Vec<Particle>,
     image: Image,
     rng: ThreadRng,
+    next_particle: usize,
 }
 
 impl Particles {
@@ -35,16 +43,20 @@ impl Particles {
             particles,
             image,
             rng: rand::thread_rng(),
+            next_particle: 0,
         }
     }
 
     pub fn draw(&mut self, window: &mut Window) {
         for particle in &mut self.particles {
             if particle.alive > 0.0 {
-                let colour = Color::GREEN.with_alpha(particle.alive / 30.0); // TODO: no magic.
+                let colour = Color::GREEN.with_alpha(particle.alive / HALF_LIFE);
                 particle.position += particle.velocity;
                 window.draw(
-                    &Rectangle::new(particle.position - Vector::new(8, 8), Vector::new(16, 16)),
+                    &Rectangle::new(
+                        particle.position - Vector::new(PARTICLE_SIZE / 2.0, PARTICLE_SIZE / 2.0),
+                        Vector::new(PARTICLE_SIZE, PARTICLE_SIZE),
+                    ),
                     Blended(&self.image, colour),
                 );
                 particle.alive -= 1.0;
@@ -53,19 +65,14 @@ impl Particles {
     }
 
     pub fn add(&mut self, number: usize, position: Vector, angle: f32) {
-        let mut number = number;
-        for particle in &mut self.particles {
-            if particle.alive <= 0.0 {
-                let angle = angle + self.rng.gen_range(-30.0, 30.0);
-                let speed = 2.0 + self.rng.gen_range(0.0, 4.0);
-                particle.position = position;
-                particle.velocity = Transform::rotate(angle) * Vector::new(0, -speed);
-                particle.alive = 60.0; // TODO: no magic.
-                number -= 1;
-                if number == 0 {
-                    break;
-                }
-            }
+        for _ in 0..number {
+            let mut particle = &mut self.particles[self.next_particle];
+            let angle = angle + self.rng.gen_range(-SPREAD_ANGLE, SPREAD_ANGLE);
+            let speed = self.rng.gen_range(MIN_SPEED, MAX_SPEED);
+            particle.position = position;
+            particle.velocity = Transform::rotate(angle) * Vector::new(0, -speed);
+            particle.alive = PARTICLE_LIFE;
+            self.next_particle = (self.next_particle + 1) % self.particles.len();
         }
     }
 }
